207eb317934d63468e88b1d3881ab2ef92e4e7b6
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-02-22 16:48:33 -0800

    pass triangle constructor test

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -6,6 +6,22 @@ namespace ShapeTracker.Tests
   [TestClass]
   public class TriangleTests
   {
-    // Tests will go here!
+    [TestMethod]
+    public void TriangleConstructor_CreatesInstanceOfTraingle_Triange()
+    // public: allows the tools running our tests to access them. 
+    // void: because this method does not return anything. 
+    // naming convention: 
+    // 1) TriangleConstructor: method being tested
+    // 2) CreatesInstanceOfTriange: behavior of method
+    // 3) Triangle: expected return value of tested method 
+    {
+      Triangle newTriangle = new Triangle();
+      // behavior being tested 
+
+      Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
+      // typeof(): type of a class 
+      // GetType(): returns data type of a specific obj 
+      // example: Assert.AreEqual(true, 1 == 1) => passed test
+    }
   }
 }
