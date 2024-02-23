7d679aedc82117e6f1348fee02002772e1d2265d
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-02-22 22:13:20 -0800

    add Side2 GET functionality + passing test

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -7,7 +7,7 @@ namespace ShapeTracker.Tests
   public class TriangleTests
   {
     [TestMethod]
-    public void TriangleConstructor_CreatesInstanceOfTraingle_Triangle()
+    public void TriangleConstructor_CreatesInstanceOfTriangle_Triangle()
     // public: allows the tools running our tests to access them. 
     // void: because this method does not return anything. 
     // naming convention: 
@@ -15,7 +15,7 @@ namespace ShapeTracker.Tests
     // 2) CreatesInstanceOfTriange: behavior of method
     // 3) Triangle: expected return value of tested method 
     {
-      Triangle newTriangle = new Triangle(2);
+      Triangle newTriangle = new Triangle(2, 3);
       // behavior being tested 
 
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
@@ -29,7 +29,7 @@ namespace ShapeTracker.Tests
     // organizational trick: arrange - act - assert 
     {
       int length1 = 3;
-      Triangle newTriangle = new Triangle(length1);
+      Triangle newTriangle = new Triangle(length1, 2);
       // arrange: gather necessary components 
 
       int result = newTriangle.Side1;
@@ -42,10 +42,19 @@ namespace ShapeTracker.Tests
     [TestMethod]
     public void SetSide1_SetsValueOfSide1_Void()
     {
-      Triangle newTriangle = new Triangle(3);
+      Triangle newTriangle = new Triangle(3, 3);
       int newLength1 = 44;
       newTriangle.Side1 = newLength1;
       Assert.AreEqual(newLength1, newTriangle.Side1);
+    }
+
+    [TestMethod]
+    public void GetSide2_ReturnsSide2_Int()
+    {
+      int length2 = 3;
+      Triangle newTriangle = new Triangle(2, length2);
+      int result = newTriangle.Side2;
+      Assert.AreEqual(length2, result);
     }
   }
 }
