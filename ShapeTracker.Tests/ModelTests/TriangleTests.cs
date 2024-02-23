afab9253878d5afe266fa24619c2bc3f62a7e159
Hayeong Pyeon <hayeong.pyeon@gmail.com>
2024-02-22 21:11:19 -0800

    add Side1 get functionality + passing test

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -7,7 +7,7 @@ namespace ShapeTracker.Tests
   public class TriangleTests
   {
     [TestMethod]
-    public void TriangleConstructor_CreatesInstanceOfTraingle_Triange()
+    public void TriangleConstructor_CreatesInstanceOfTraingle_Triangle()
     // public: allows the tools running our tests to access them. 
     // void: because this method does not return anything. 
     // naming convention: 
@@ -15,13 +15,28 @@ namespace ShapeTracker.Tests
     // 2) CreatesInstanceOfTriange: behavior of method
     // 3) Triangle: expected return value of tested method 
     {
-      Triangle newTriangle = new Triangle();
+      Triangle newTriangle = new Triangle(2);
       // behavior being tested 
 
       Assert.AreEqual(typeof(Triangle), newTriangle.GetType());
       // typeof(): type of a class 
       // GetType(): returns data type of a specific obj 
       // example: Assert.AreEqual(true, 1 == 1) => passed test
+    }
+
+    [TestMethod]
+    public void GetSide1_ReturnsSide1_Int()
+    // organizational trick: arrange - act - assert 
+    {
+      int length1 = 3;
+      Triangle newTriangle = new Triangle(length1);
+      // arrange: gather necessary components 
+
+      int result = newTriangle.Side1;
+      // act: invoke functionality by calling a method 
+
+      Assert.AreEqual(length1, result);
+      // assert: confirm functionality by comparing actual and anticipated outputs
     }
   }
 }
